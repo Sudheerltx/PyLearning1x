5e10e208d5c602fa1add9a3671a084ccf2c60c89
Sudheerltx <sudheer.d25@gmail.com>
2023-10-13 09:47:15 +0530

    Task

--- PyLearning1x/13_OCT_2023/Task3.py
+++ PyLearning1x/13_OCT_2023/Task3.py
@@ -3,9 +3,8 @@ a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 c = int(input("Enter third number:"))
 
-tmp = (a if a > b else b)
-ma = (tmp if tmp > c else c)
-print("Maximum of ", a, b, c, "is", ma)
+max1 = (a if a > b and a > c else b if b > a and b > c else c)
+print("Maximum of ", a, b, c, "is :", max1)
 
 # 2. Develop a Python script that calculates the square and cube of a given number.
 num = int(input("Enter a number:"))
